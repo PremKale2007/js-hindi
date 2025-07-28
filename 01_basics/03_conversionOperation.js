33c39fdbe41601e869a9f7e5ee2135e3bf3e5a17
Prem Kale <Premkale1563@gmail.com>
2025-07-28 07:11:11 +0000

    datatypes summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score="33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);    //not a number
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);    //not a number
 
 // "33"=>33
 // "33abc"=>NaN
@@ -15,7 +15,7 @@ console.log(valueInNumber);    //not a number
 let isLoggedIn=1
 
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1=>true
 // 0=>false
@@ -26,4 +26,42 @@ console.log(booleanIsLoggedIn);
 let someNumber=33
 
 let stringNumber=String(someNumber)
-console.log(stringNumber);
+//console.log(stringNumber);
+
+//***********************************Operations **************************************
+
+let value=3
+let negvalue=-value
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%2);
+
+let str1="hello"
+let str2=" hitesh"
+
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+console.log(3+4*5%3);
+
+console.log(+true);  //1
+console.log(+"");  //0
+
+//let num1,num2,num3
+
+//num=1=num2=num=3=2+2
+
+let gameCounter=100
+console.log(++gameCounter);
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2==1);
+//console.log(2!=1);
+
+//console.log("2">1);
+//console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);   //true because comparison operator assums null as 0
+
+
+console.log(undefined==0);
+
+//  ===  strict check
+
+console.log("2"===2);   // false because ==== also check the data types
+
+
+console.log("2">true);   // true ,it converts true as an 1
+
+
