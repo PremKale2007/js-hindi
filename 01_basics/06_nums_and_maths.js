491e605fcb4925909c3ff3992f9015525950954b
Prem Kale <Premkale1563@gmail.com>
2025-07-29 12:24:02 +0000

    Maths

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -16,5 +16,23 @@ const hundreds=1000000
 
 
 //*********************************MATHS*******************************
-
+/*
 console.log(Math);
+console.log(Math.abs(-4));     // it makes negative value positive
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+console.log(Math.min(4,3,7,5));
+console.log(Math.max(4,3,7,5));
+*/
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max-min+1))+min)
+
